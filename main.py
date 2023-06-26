cb195a903a26f2f2f0896e86d83c8cfb154a99cf
Ray <74573089+RayHyper@users.noreply.github.com>
2023-06-26 04:07:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pyautogui
 import win32api
 import win32con
-from pymsgbox import alert
+import PySimpleGUI as sg
 
 
-alert(text='Aim Trainer Bot', title='Aimbot', button='Confirm')
+sg.theme('LightBlue6')
+sg.set_options(font=('Futura', 20))
+layout = [[sg.Text("Aim Trainer Bot V1")],[sg.Text("https://aimtrainer.io/challenge")],[sg.Button("Start")]]
+window = sg.Window("Bot",layout,element_justification="c",size=(500,200))
+event, values = window.read()
+window.close()
+
 
 play = pyautogui.locateCenterOnScreen("play.PNG", confidence=0.65, grayscale=True)
-pyautogui.moveTo(play)
-pyautogui.leftClick()
+if play:
+    pyautogui.moveTo(play)
+    pyautogui.leftClick()
 
+else:
+    sg.theme("LightBlue")
+    sg.popup_ok("Error", "Aim Trainer Not Found", title="Error")
+    exit(1)
 
 def click(x, y):
     win32api.SetCursorPos((x, y))
@@ -17,7 +28,12 @@ def click(x, y):
     #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 while True:
+
     #disk = pyautogui.locateCenterOnScreen("realaim.PNG", confidence=0.90, grayscale=True,region = (561,361,1358,976))
-    disk = pyautogui.locateCenterOnScreen("aim.PNG", confidence=0.60, grayscale=True)
+    disk = pyautogui.locateCenterOnScreen("aim.PNG", confidence=0.65, grayscale=True)
+
     if disk:
         click(disk[0], disk[1])
+
+
+
