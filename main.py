1ee384a626a30da4a2f2abd9c8176105db6afb6b
Ray <74573089+RayHyper@users.noreply.github.com>
2023-06-26 02:30:56 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pyautogui
+import win32api
+import win32con
 from pymsgbox import alert
 
-import mouse
 
 alert(text='Aim Trainer Bot', title='Aimbot', button='Confirm')
 
+play = pyautogui.locateCenterOnScreen("play.PNG", confidence=0.65, grayscale=True)
+pyautogui.moveTo(play)
+pyautogui.leftClick()
+
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
 while True:
-    disk = pyautogui.locateCenterOnScreen("aim.png", confidence = 0.70, grayscale = True)
-    pyautogui.moveTo(disk)
-    pyautogui.leftClick()
-
-
-
-
-
-
-
-
+    #disk = pyautogui.locateCenterOnScreen("realaim.PNG", confidence=0.90, grayscale=True,region = (561,361,1358,976))
+    disk = pyautogui.locateCenterOnScreen("aim.PNG", confidence=0.60, grayscale=True)
+    if disk:
+        click(disk[0], disk[1])
